6a698ab39e55da559673971a572370d1067e8969
Amir Akhter Kazi <kazi@uchicago.edu>
2020-05-28 12:54:51 -0500

    Made changes to cleanup file

--- Cleaning_Backups.py
+++ Cleaning_Backups.py
@@ -1,13 +1,22 @@
+'''
+Use: Delete .tsbak tableau files that are out-dated and unnecessary
+    using certain filters such as number of days elapsed, and the
+    minimum number of backups
+
+Written by: Amir Kazi
+Date: 28th May 2020
+'''
 import os
 import time
 
-# directory = "E:\Tableau_Backups"
-directory = "E:\Backups"
+# Change below 3 variables for any future changes
+directory = "E:\Tableau_Backups"
+Number_of_days_since_today = 4
+Minimum_number_of_backups = 4
 
 if os.path.exists(directory) :
     os.chdir(directory)
     print("Current Working Directory " , os.getcwd())
-
 
     list_of_backups = []
     for file in os.listdir():
@@ -15,13 +24,12 @@ if os.path.exists(directory) :
             list_of_backups.append(file)
 
     # Calculates epoch time to find .tsbak files beyond a certain date
-    Number_of_days_since_today = 1
-    Minimum_number_of_backups = 4
     now = time.time()
-    for file in os.listdir():
-        if file.endswith('tsbak') and len(list_of_backups) >= Minimum_number_of_backups and \
+    for file in list_of_backups:
+        if len(list_of_backups) >= Minimum_number_of_backups and \
             os.stat(file).st_ctime < now - (Number_of_days_since_today * 86400):
-            print (file)
-
+            os.remove(file)
+            print("File Removed: ", file)   
+            
 else:
     print("The directory does not exist.")  
